606ddc312f092eadbbfff696f79af8980d6717eb
Emily Coco Parkes <emilyparkes@Willow.local>
2019-07-08 19:06:43 +1200

    '.references()' on one line with column name

--- server/db/migrations/20190704164242_water_usages.js
+++ server/db/migrations/20190704164242_water_usages.js
@@ -1,8 +1,7 @@
 exports.up = knex =>
   knex.schema.createTable('water_usages', (table) => {
     table.increments('id').primary()
-    table.integer('food_id')
-      .references('foods.id')
+    table.integer('food_id').references('foods.id')
     table.decimal('value', null, 2)
   })
 
