a53b33de998450cb8f1d0822e0358ae5b543be3a
jess morgan <jess-morgan1@outlook.com>
2019-07-10 10:48:17 +1200

    styling

--- client/components/Food.jsx
+++ client/components/Food.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
-import { Grid } from 'semantic-ui-react'
+import { Table } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
 
 export default function Food ({ food: { name } }) {
   return (
     <>
-     <Grid reversed='mobile vertically'>
-       <Grid.Row>
-         <Link to='#'><Grid.Column>{name}</Grid.Column></Link>
-       </Grid.Row>
-     </Grid>
+<Table singleLine style={{ width: '20vw' }}>
+  <Table.Header>
+    <Table.Row >
+      <Table.HeaderCell> <Link to='#'>{name}</Link></Table.HeaderCell>
+    </Table.Row>
+  </Table.Header>
+</Table>
+
     </>
   )
 }
