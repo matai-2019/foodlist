8c92d6a55da4d56ae7261148d375d6bc96a8d9fc
Bryce Kehoe <bryce@kehoe.co.nz>
2019-07-09 19:29:49 +1200

    Changed 'value' column from decimal to integer in 'water_usages' table

--- server/db/migrations/20190704164242_water_usages.js
+++ server/db/migrations/20190704164242_water_usages.js
@@ -2,7 +2,7 @@ exports.up = knex =>
   knex.schema.createTable('water_usages', (table) => {
     table.increments('id').primary()
     table.integer('food_id').references('foods.id')
-    table.decimal('value', null, 2)
+    table.integer('value')
   })
 
 exports.down = knex => knex.schema.dropTable('water_usages')
