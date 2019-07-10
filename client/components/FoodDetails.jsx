1a47b4af4059b3b5215d545ddef9d6c5460888e4
Noel Soong <noelsoong@live.com>
2019-07-11 10:59:50 +1200

    Import Semantic UI components

---
+++ client/components/FoodDetails.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Grid, Container, Card } from 'semantic-ui-react'
+
+function FoodDetails () {
+  return (
+    <Grid>
+      <Container>
+        <Card>
+
+        </Card>
+      </Container>
+    </Grid>
+  )
+}
+
+export default FoodDetails
