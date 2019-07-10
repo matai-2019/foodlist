4ea00fe6da94edef3f55a230cd782ae47a5e8296
Bryce Kehoe <bryce@kehoe.co.nz>
2019-07-10 14:29:23 +1200

    Formatting fixes

--- client/components/Food.jsx
+++ client/components/Food.jsx
@@ -5,14 +5,13 @@ import { Link } from 'react-router-dom'
 export default function Food ({ food: { name } }) {
   return (
     <>
-<Table singleLine style={{ width: '20vw' }}>
-  <Table.Header>
-    <Table.Row >
-      <Table.HeaderCell> <Link to='#'>{name}</Link></Table.HeaderCell>
-    </Table.Row>
-  </Table.Header>
-</Table>
-
+      <Table singleLine style={{ width: '20vw' }}>
+        <Table.Header>
+          <Table.Row >
+            <Table.HeaderCell> <Link to='#'>{name}</Link></Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+      </Table>
     </>
   )
 }
