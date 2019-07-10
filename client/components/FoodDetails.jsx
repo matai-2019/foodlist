fb68666400ef08383d84293c8b99d9827e1afadb
Noel Soong <noelsoong@live.com>
2019-07-11 11:00:15 +1200

    Mock Data for foodDetail

--- client/components/FoodDetails.jsx
+++ client/components/FoodDetails.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
 import { Grid, Container, Card } from 'semantic-ui-react'
 
+const foodDetails = {
+  'name': 'Turkey',
+  'category': 'meat',
+  'carbon_output': 403,
+  'water_usage': 52
+}
+
 function FoodDetails () {
   return (
     <Grid>
