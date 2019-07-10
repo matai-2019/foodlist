73d34b6a078c782079bdb6d7c97b6aab9ce8b756
Bryce Kehoe <bryce@kehoe.co.nz>
2019-07-10 14:29:57 +1200

    Remove Link from Food component (ticket exists for this)

--- client/components/Food.jsx
+++ client/components/Food.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Table } from 'semantic-ui-react'
-import { Link } from 'react-router-dom'
 
 export default function Food ({ food: { name } }) {
   return (
@@ -8,7 +7,7 @@ export default function Food ({ food: { name } }) {
       <Table singleLine style={{ width: '20vw' }}>
         <Table.Header>
           <Table.Row >
-            <Table.HeaderCell> <Link to='#'>{name}</Link></Table.HeaderCell>
+            <Table.HeaderCell>{name}</Table.HeaderCell>
           </Table.Row>
         </Table.Header>
       </Table>
