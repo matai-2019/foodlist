97bd956d83722f3ced4035288c8153c4f90d9e02
PhilipJi1608 <phil12p345@gmail.com>
2019-07-11 11:16:31 +1200

    Revert "Created links in Food component to FoodDetails"
    This reverts commit 4eb51c2708936528d6a2ded6b1f91617865abfc6.

--- client/components/Food.jsx
+++ client/components/Food.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import { Table } from 'semantic-ui-react'
-import { Link } from 'react-router-dom'
 
-export default function Food ({ food: { name, id } }) {
+export default function Food ({ food: { name } }) {
   return (
     <>
       <Table singleLine>
         <Table.Header>
           <Table.Row >
-            <Table.HeaderCell><Link to={'/details/' + id}>{name}</Link></Table.HeaderCell>
+            <Table.HeaderCell>{name}</Table.HeaderCell>
           </Table.Row>
         </Table.Header>
       </Table>
