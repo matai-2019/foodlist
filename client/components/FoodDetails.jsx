8a78dca4a31e104650f6b8aff7765f76a36f70ec
Noel Soong <noelsoong@live.com>
2019-07-11 11:18:31 +1200

    First iteration of Food Details page

--- client/components/FoodDetails.jsx
+++ client/components/FoodDetails.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Grid, Container, Card } from 'semantic-ui-react'
+import { Grid, Container, Card, Divider, Statistic, GridColumn } from 'semantic-ui-react'
 
 const foodDetails = {
   'name': 'Turkey',
@@ -13,7 +13,39 @@ function FoodDetails () {
     <Grid>
       <Container>
         <Card>
-
+          <Card.Content>
+            <Card.Header>
+              {foodDetails.name}
+            </Card.Header>
+            <Card.Description>
+              <Grid>
+                <Grid.Column>
+                  <Statistic>
+                    <Statistic.Value>
+                      {foodDetails.carbon_output}
+                    </Statistic.Value>
+                    <Statistic.Label>
+                      Carbon Output
+                    </Statistic.Label>
+                  </Statistic>
+                </Grid.Column>
+                <Grid.Column>
+                  <Statistic>
+                    <Statistic.Value>
+                      {foodDetails.water_usage}
+                    </Statistic.Value>
+                    <Statistic.Label>
+                      Water Usage
+                    </Statistic.Label>
+                  </Statistic>
+                </Grid.Column>
+              </Grid>
+              <Divider vertical></Divider>
+            </Card.Description>
+          </Card.Content>
+          <Card.Content extra>
+            Category: {foodDetails.category}
+          </Card.Content>
         </Card>
       </Container>
     </Grid>
