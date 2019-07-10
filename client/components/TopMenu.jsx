54a55e76bdeda29d8b463e0866885ecdde74cf18
Bryce Kehoe <bryce@kehoe.co.nz>
2019-07-10 20:36:10 +1200

    Remove unused links from TopMenu

--- client/components/TopMenu.jsx
+++ client/components/TopMenu.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { Container, Header, Menu, Input } from 'semantic-ui-react'
+import { Container, Header, Menu } from 'semantic-ui-react'
 
 export default function TopMenu () {
   return (
@@ -10,13 +10,6 @@ export default function TopMenu () {
           <Header as='h1' inverted>The Food List</Header>
         </Menu.Item>
         <Menu.Item as={Link} to='/'>Home</Menu.Item>
-        <Menu.Item as={Link} to='/'>A-Z</Menu.Item>
-        <Menu.Item as={Link} to='/'>CO2</Menu.Item>
-        <Menu.Menu position='right'>
-          <Menu.Item>
-            <Input icon='search' placeholder='Search...' />
-          </Menu.Item>
-        </Menu.Menu>
       </Container>
     </Menu>
   )
