99dead144e7c85456bc70a524ad36321f60a012b
Bryce Kehoe <bryce@kehoe.co.nz>
2019-07-10 20:50:09 +1200

    food_group ➡️ category

--- server/db/migrations/20190704164159_foods.js
+++ server/db/migrations/20190704164159_foods.js
@@ -2,7 +2,7 @@ exports.up = (knex) =>
   knex.schema.createTable('foods', (table) => {
     table.increments('id').primary()
     table.string('name')
-    table.integer('food_group_id').references('food_groups.id')
+    table.integer('category_id').references('categories.id')
   })
 
 exports.down = (knex) => knex.schema.dropTable('foods')
--- server/db/seeds/foods.js
+++ server/db/seeds/foods.js
@@ -1,8 +1,8 @@
 exports.seed = (knex) =>
   knex('foods').del()
-    .then(() => knex('food_groups').del())
+    .then(() => knex('categories').del())
     .then(() =>
-      knex('food_groups').insert([
+      knex('categories').insert([
         { id: 1, name: 'Fruits' },
         { id: 2, name: 'Vegetables' },
         { id: 3, name: 'Grains, beans, and legumes' },
@@ -12,31 +12,31 @@ exports.seed = (knex) =>
       ]))
     .then(() =>
       knex('foods').insert([
-        { id: 1, name: 'Lamb', food_group_id: 5 },
-        { id: 2, name: 'Beef', food_group_id: 5 },
-        { id: 3, name: 'Turkey', food_group_id: 5 },
-        { id: 4, name: 'Broccoli', food_group_id: 2 },
-        { id: 5, name: 'Tuna', food_group_id: 4 },
-        { id: 6, name: 'Salmon', food_group_id: 4 },
-        { id: 7, name: 'Cheese', food_group_id: 6 },
-        { id: 8, name: 'Pork', food_group_id: 5 },
-        { id: 9, name: 'Greek Yogurt', food_group_id: 6 },
-        { id: 10, name: 'Chicken', food_group_id: 5 },
-        { id: 11, name: 'Milk', food_group_id: 6 },
-        { id: 12, name: 'Eggs', food_group_id: 6 },
-        { id: 13, name: 'Rice', food_group_id: 3 },
-        { id: 14, name: 'Potatoes', food_group_id: 2 },
-        { id: 15, name: 'Beans', food_group_id: 3 },
-        { id: 16, name: 'Tomatoes', food_group_id: 1 },
-        { id: 17, name: 'Tofu', food_group_id: 3 },
-        { id: 18, name: 'Lentils', food_group_id: 3 },
-        { id: 19, name: 'Peanut Butter', food_group_id: 3 },
-        { id: 20, name: 'Nuts', food_group_id: 3 },
-        { id: 21, name: 'Oranges', food_group_id: 1 },
-        { id: 22, name: 'Apples', food_group_id: 1 },
-        { id: 23, name: 'Corn', food_group_id: 2 },
-        { id: 24, name: 'Lettuce', food_group_id: 2 },
-        { id: 25, name: 'Strawberries', food_group_id: 1 },
-        { id: 26, name: 'Avocados', food_group_id: 1 },
-        { id: 27, name: 'Plum', food_group_id: 1 }
+        { id: 1, name: 'Lamb', category_id: 5 },
+        { id: 2, name: 'Beef', category_id: 5 },
+        { id: 3, name: 'Turkey', category_id: 5 },
+        { id: 4, name: 'Broccoli', category_id: 2 },
+        { id: 5, name: 'Tuna', category_id: 4 },
+        { id: 6, name: 'Salmon', category_id: 4 },
+        { id: 7, name: 'Cheese', category_id: 6 },
+        { id: 8, name: 'Pork', category_id: 5 },
+        { id: 9, name: 'Greek Yogurt', category_id: 6 },
+        { id: 10, name: 'Chicken', category_id: 5 },
+        { id: 11, name: 'Milk', category_id: 6 },
+        { id: 12, name: 'Eggs', category_id: 6 },
+        { id: 13, name: 'Rice', category_id: 3 },
+        { id: 14, name: 'Potatoes', category_id: 2 },
+        { id: 15, name: 'Beans', category_id: 3 },
+        { id: 16, name: 'Tomatoes', category_id: 1 },
+        { id: 17, name: 'Tofu', category_id: 3 },
+        { id: 18, name: 'Lentils', category_id: 3 },
+        { id: 19, name: 'Peanut Butter', category_id: 3 },
+        { id: 20, name: 'Nuts', category_id: 3 },
+        { id: 21, name: 'Oranges', category_id: 1 },
+        { id: 22, name: 'Apples', category_id: 1 },
+        { id: 23, name: 'Corn', category_id: 2 },
+        { id: 24, name: 'Lettuce', category_id: 2 },
+        { id: 25, name: 'Strawberries', category_id: 1 },
+        { id: 26, name: 'Avocados', category_id: 1 },
+        { id: 27, name: 'Plums', category_id: 1 }
       ]))
