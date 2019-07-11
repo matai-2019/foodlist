3f3a38fa7161cbf6b68bdb5de58de34a3fae3575
Keith Yoon <uniyoona@naver.com>
2019-07-11 15:09:26 +1200

    create database function editFood()

--- server/db/foods.js
+++ server/db/foods.js
@@ -4,6 +4,30 @@ function getFoods (db = connection) {
   return db('foods')
 }
 
+function editFood (food, db = connection) {
+  return db('foods')
+    .where('id', food.id)
+    .update({
+      name: food.name,
+      category_id: food.category_id
+    })
+    .then(() => {
+      return db('carbon_outputs')
+        .where('food_id', food.id)
+        .update({
+          value: food.carbon_output
+        })
+    })
+    .then(() => {
+      return db('water_usages')
+        .where('food_id', food.id)
+        .update({
+          value: food.water_usage
+        })
+    })
+}
+
 module.exports = {
-  getFoods
+  getFoods,
+  editFood
 }
