d1666ebc2495a89338ba4409ba54dbf638491bc1
Jared Tasker <jared@needapart.co.nz>
2019-07-11 18:06:57 +1200

    Update editFood() so that columns can be omitted from the update

--- server/db/foods.js
+++ server/db/foods.js
@@ -8,22 +8,31 @@ function editFood (food, db = connection) {
   return db('foods')
     .where('id', food.id)
     .update({
+      id: food.id,
       name: food.name,
       category_id: food.category_id
     })
     .then(() => {
-      return db('carbon_outputs')
-        .where('food_id', food.id)
-        .update({
-          value: food.carbon_output
-        })
+      if (food.carbon_output) {
+        return db('carbon_outputs')
+          .where('food_id', food.id)
+          .update({
+            value: food.carbon_output
+          })
+      } else {
+        return db
+      }
     })
     .then(() => {
-      return db('water_usages')
-        .where('food_id', food.id)
-        .update({
-          value: food.water_usage
-        })
+      if (food.water_usage) {
+        return db('water_usages')
+          .where('food_id', food.id)
+          .update({
+            value: food.water_usage
+          })
+      } else {
+        return db
+      }
     })
     .then(() => db)
     .then(getFoods)
