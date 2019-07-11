46f3afbcb52e802abb30a0883a5752719e5aa225
Bryce Kehoe <bryce@kehoe.co.nz>
2019-07-11 18:51:53 +1200

    Match dev

--- client/components/Food.jsx
+++ client/components/Food.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Table } from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
 
-export default function Food ({ food: { name } }) {
+export default function Food ({ food: { name, id } }) {
   return (
     <>
       <Table singleLine>
         <Table.Header>
           <Table.Row >
-            <Table.HeaderCell>{name}</Table.HeaderCell>
+            <Table.HeaderCell><Link to={'/details/' + id }>{name}</Link></Table.HeaderCell>
           </Table.Row>
         </Table.Header>
       </Table>
