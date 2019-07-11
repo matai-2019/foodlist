3eaa0b9f81f995c15f8097bcb0ff505a199d0d85
Rahul Lal <Rahul21Lal@gmail.com>
2019-07-11 13:41:31 +1200

    router.post intialistation

--- server/db/foods.js
+++ server/db/foods.js
@@ -4,6 +4,12 @@ function getFoods (db = connection) {
   return db('foods')
 }
 
+function addFood (food, db = connection) {
+  return db('foods')
+    .insert(food)
+}
+
 module.exports = {
-  getFoods
+  getFoods,
+  addFood
 }
--- server/routes/foods.js
+++ server/routes/foods.js
@@ -9,4 +9,11 @@ router.get('/', (req, res) => {
     .then(foods => res.status(200).json(foods))
 })
 
+router.post('/', (req, res) => {
+  const foods = req.body
+  console.log('api', foods)
+  db.addFood(foods)
+    .then(id => res.status(201).send(foods))
+})
+
 module.exports = router
