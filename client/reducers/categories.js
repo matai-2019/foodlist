d55df343e1c3fd8e9f2aae69b44b86db5e2d8b28
Ashley Foster <ajp.foster@gmail.com>
2019-07-12 10:53:30 +1200

    refactor foods actions out

--- client/reducers/categories.js
+++ client/reducers/categories.js
@@ -1,9 +1,4 @@
 import { GET_CATEGORIES_SUCCESS, GET_CATEGORIES_PENDING, GET_CATEGORIES_ERROR } from './../actions/categories'
-import {
-  GET_FOODS_PENDING,
-  GET_FOODS_SUCCESS,
-  GET_FOODS_ERROR
-} from './../actions/foods'
 
 export default function categoriesReducer (state = [], action) {
   switch (action.type) {
@@ -21,26 +16,6 @@ export default function categoriesReducer (state = [], action) {
       }
 
     case GET_CATEGORIES_ERROR:
-      return {
-        ...state,
-        pending: false,
-        error: action.message
-      }
-
-    case GET_FOODS_PENDING:
-      return {
-        ...state,
-        pending: true
-      }
-
-    case GET_FOODS_SUCCESS:
-      return {
-        ...state,
-        pending: false,
-        error: null
-      }
-
-    case GET_FOODS_ERROR:
       return {
         ...state,
         pending: false,
