89d9a24cb827136e92cb23431807f166be07ebb5
Ashley Foster <ajp.foster@gmail.com>
2019-07-12 11:47:06 +1200

    add initial state and actions to info reducer

--- client/reducers/categories.js
+++ client/reducers/categories.js
@@ -1,26 +1,9 @@
-import { GET_CATEGORIES_SUCCESS, GET_CATEGORIES_PENDING, GET_CATEGORIES_ERROR } from './../actions/categories'
+import { GET_CATEGORIES_SUCCESS } from './../actions/categories'
 
 export default function categoriesReducer (state = [], action) {
   switch (action.type) {
-    case GET_CATEGORIES_PENDING:
-      return {
-        ...state,
-        pending: true
-      }
-
     case GET_CATEGORIES_SUCCESS:
-      return {
-        ...state,
-        pending: false,
-        error: null
-      }
-
-    case GET_CATEGORIES_ERROR:
-      return {
-        ...state,
-        pending: false,
-        error: action.message
-      }
+      return action.categories
 
     default:
       return state
--- client/reducers/info.js
+++ client/reducers/info.js
@@ -1,17 +1,26 @@
 import {
   GET_FOODS_PENDING,
   GET_FOODS_SUCCESS,
-  GET_FOODS_ERROR } from './../actions/foods'
+  GET_FOODS_ERROR
+} from './../actions/foods'
+
+import {
+  GET_CATEGORIES_PENDING,
+  GET_CATEGORIES_SUCCESS,
+  GET_CATEGORIES_ERROR
+} from '../actions/categories'
 
 export default function infoReducer (state = { pending: true }, action) {
   switch (action.type) {
     case GET_FOODS_PENDING:
+    case GET_CATEGORIES_PENDING:
       return {
         ...state,
         pending: true
       }
 
     case GET_FOODS_SUCCESS:
+    case GET_CATEGORIES_SUCCESS:
       return {
         ...state,
         pending: false,
@@ -19,6 +28,7 @@ export default function infoReducer (state = { pending: true }, action) {
       }
 
     case GET_FOODS_ERROR:
+    case GET_CATEGORIES_ERROR:
       return {
         ...state,
         pending: false,
