185c0816bd468b7f4366f95971752b1b43435be4
Tamari Jacob Gray-Kaiwahia <tamarigray97@gmail.com>
2019-07-11 17:13:24 +1200

    issues with test

--- tests/server/routes/foods.test.js
+++ tests/server/routes/foods.test.js
@@ -1,4 +1,5 @@
 import request from 'supertest'
+import { Item } from 'semantic-ui-react'
 
 jest.mock('../../../server/db/foods', () => ({
   getFoods: () => Promise.resolve([
@@ -17,5 +18,20 @@ test('GET /foods returns all of the foods', () => {
     .expect(200)
     .then(res => {
       expect(res.body).toHaveLength(3)
+    })
+})
+
+test('POST adds a new food', () => {
+  const newFood = {
+    name: 'Mungo',
+    category_id: 1,
+    carbon_output: 142,
+    water_usage: 69
+  }
+  return request(server)
+    .post('/api/v1/foods')
+    .send(newFood)
+    .then(res => {
+      expect(res.body.id).toBe(28)
     })
 })
