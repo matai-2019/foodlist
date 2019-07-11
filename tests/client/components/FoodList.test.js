18efeef60a6149aed35e92a0eac67555a6c996ff
Anisha Dhana <anishadhana@gmail.com>
2019-07-11 15:35:57 +1200

    Fixed issues requested by PR

--- tests/client/components/FoodList.test.js
+++ tests/client/components/FoodList.test.js
@@ -1,5 +1,4 @@
 import React from 'react'
-// import { shallow } from 'enzyme'
 import { render } from 'enzyme'
 import { Provider } from 'react-redux'
 import configureStore from 'redux-mock-store'
