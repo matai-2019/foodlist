3fef0e0d6f8bab49302eef942416f2a31d25acbe
Ashley Foster <ajp.foster@gmail.com>
2019-07-11 14:06:36 +1200

    modified getFood to get joined tables

--- server/db/foods.js
+++ server/db/foods.js
@@ -6,8 +6,18 @@ function getFoods (db = connection) {
 
 function getFood (id, db = connection) {
   return db('foods')
-    .where('id', id)
+    .join('water_usages', 'foods.id', 'water_usages.food_id')
+    .join('carbon_outputs', 'foods.id', 'carbon_outputs.food_id')
+    .join('categories', 'foods.category_id', 'categories.id')
+    .where('foods.id', id)
     .first()
+    .select(
+      'foods.id',
+      'foods.name',
+      'categories.name as category',
+      'carbon_outputs.value as carbon_output',
+      'water_usages.value as water_usage'
+    )
 }
 
 module.exports = {
