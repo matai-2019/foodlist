9079886a0f1594017f71a5cfbf377c9f4a6e632f
Rahul Lal <Rahul21Lal@gmail.com>
2019-07-11 14:55:49 +1200

    Completed addFood function

--- server/db/foods.js
+++ server/db/foods.js
@@ -5,8 +5,20 @@ function getFoods (db = connection) {
 }
 
 function addFood (food, db = connection) {
+  let foodID
+
   return db('foods')
-    .insert(food)
+    .insert({ name: food.name, category_id: food.category_id })
+    .then((id) => {
+      foodID = id[0]
+      return db('carbon_outputs')
+        .insert({ food_id: foodID, value: food.carbon_output })
+    })
+    .then((id) => {
+      return db('water_usages')
+        .insert({ food_id: foodID, value: food.water_usage })
+    })
+    .catch(err => err)
 }
 
 module.exports = {
