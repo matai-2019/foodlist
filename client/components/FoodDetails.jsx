019bc5506d0c243321765fa2a840316b157c5266
Noel Soong <noelsoong@live.com>
2019-07-11 19:43:43 +1200

    Convert to Class Component

--- client/components/FoodDetails.jsx
+++ client/components/FoodDetails.jsx
@@ -1,49 +1,59 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import { Grid, Container, Card, Statistic, Icon } from 'semantic-ui-react'
+// import { getFoodDetails } from '../actions'
 
-function FoodDetails ({ foodId, foodDetails }) {
-  return (
-    <Grid className="food-details">
-      <Container>
-        <Card centered>
-          <Card.Content>
-            <Card.Header>
-              {foodDetails.name}
-            </Card.Header>
-            <Card.Description>
-              <Grid columns={2} divided>
-                <Grid.Column>
-                  <Statistic>
-                    <Icon color="grey" name="cloud" size="huge"/>
-                    <Statistic.Value>
-                      {foodDetails.carbon_output}
-                    </Statistic.Value>
-                    <Statistic.Label>
-                      Carbon Output
-                    </Statistic.Label>
-                  </Statistic>
-                </Grid.Column>
-                <Grid.Column>
-                  <Statistic>
-                    <Icon color="blue" name="tint" size="huge"/>
-                    <Statistic.Value>
-                      {foodDetails.water_usage}
-                    </Statistic.Value>
-                    <Statistic.Label>
-                      Water Usage
-                    </Statistic.Label>
-                  </Statistic>
-                </Grid.Column>
-              </Grid>
-            </Card.Description>
-          </Card.Content>
-          <Card.Content extra>
-            Category: {foodDetails.category}
-          </Card.Content>
-        </Card>
-      </Container>
-    </Grid>
-  )
+class FoodDetails extends React.Component {
+  render () {
+    return (
+      <div>
+        <Container className='food-details'>
+          <Card centered>
+            <Card.Content>
+              <Card.Header>
+                {this.props.foodDetails.name}
+              </Card.Header>
+              <Card.Description>
+                <Grid columns={2} divided>
+                  <Grid.Column>
+                    <Statistic>
+                      <Icon color='grey' name='cloud' size='huge' />
+                      <Statistic.Value>
+                        {this.props.foodDetails.carbon_output}
+                      </Statistic.Value>
+                      <Statistic.Label>
+                        Carbon Output
+                      </Statistic.Label>
+                    </Statistic>
+                  </Grid.Column>
+                  <Grid.Column>
+                    <Statistic>
+                      <Icon color="blue" name='tint' size='huge' />
+                      <Statistic.Value>
+                        {this.props.foodDetails.water_usage}
+                      </Statistic.Value>
+                      <Statistic.Label>
+                        Water Usage
+                      </Statistic.Label>
+                    </Statistic>
+                  </Grid.Column>
+                </Grid>
+              </Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+              Category: {this.props.foodDetails.category}
+            </Card.Content>
+          </Card>
+        </Container>
+      </div>
+    )
+  }
 }
 
-export default FoodDetails
+const mapStateToProps = state => {
+  return {
+    foodDetails: state.foodDetails
+  }
+}
+
+export default connect(mapStateToProps)(FoodDetails)
