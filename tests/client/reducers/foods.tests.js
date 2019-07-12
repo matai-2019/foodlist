038e43a49300360b156ce278a57985678e761a43
Rahul Lal <Rahul21Lal@gmail.com>
2019-07-12 17:35:02 +1200

    Middle of doing food.test.js

---
+++ tests/client/reducers/foods.tests.js
@@ -0,0 +1,9 @@
+import { GET_CATEGORY_SUCCESS } from '..//../../client/actions/category'
+import category from '../../../client/reducers/foods'
+
+describe('jest', () => {
+  it('jest is working', () => {
+    expect(true).toBe(true)
+  })
+})
+
