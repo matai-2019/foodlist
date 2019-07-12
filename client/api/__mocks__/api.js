f86b59aa21288dc35bb3dadc0f03372b1af9d519
Tamari Jacob Gray-Kaiwahia <tamarigray97@gmail.com>
2019-07-12 19:24:11 +1200

    edit food client side api test

--- client/api/__mocks__/api.js
+++ client/api/__mocks__/api.js
@@ -3,7 +3,13 @@ export function addFood () {
 }
 
 export function editFood () {
-  return Promise.resolve({ body: 'Put ok!' })
+  return Promise.resolve({
+    id: 5,
+    name: 'black',
+    category: 'Fish',
+    carbon_output: 5.26,
+    water_usage: null
+  })
 }
 
 export function deleteFood () {
--- tests/client/api.test.js
+++ tests/client/api.test.js
@@ -1,4 +1,4 @@
-import { deleteFood, addFood } from '../../client/api/__mocks__/api'
+import { deleteFood, addFood, editFood } from '../../client/api/__mocks__/api'
 
 test('deleteFood returns Okay response', () => {
   const id = 1
@@ -14,5 +14,24 @@ test('addFood returns Post ok response', () => {
   }
   const expected = { id: 1 }
   const actual = addFood(food)
+  return expect(actual).resolves.toStrictEqual(expected)
+})
+
+test('editFood returns edited food object as response', () => {
+  const food = {
+    id: 5,
+    name: 'white',
+    category: 'pork',
+    carbon_output: 5.26,
+    water_usage: null
+  }
+  const expected = {
+    id: 5,
+    name: 'black',
+    category: 'Fish',
+    carbon_output: 5.26,
+    water_usage: null
+  }
+  const actual = editFood(food)
   return expect(actual).resolves.toStrictEqual(expected)
 })
