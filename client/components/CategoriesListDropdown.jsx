632daaea46b8524ed549320e468524c64ca04512
Bryce Kehoe <bryce@kehoe.co.nz>
2019-07-12 19:58:59 +1200

    Made mock data match seed data

--- client/components/CategoriesListDropdown.jsx
+++ client/components/CategoriesListDropdown.jsx
@@ -11,10 +11,10 @@ class CategoriesListDropdown extends React.Component {
         <Dropdown.Menu>
           <Dropdown.Item>Fruits</Dropdown.Item>
           <Dropdown.Item>Vegetables</Dropdown.Item>
-          <Dropdown.Item>Grains, Beans, and Legumes</Dropdown.Item>
+          <Dropdown.Item>Grains, beans, and legumes</Dropdown.Item>
           <Dropdown.Item>Fish</Dropdown.Item>
           <Dropdown.Item>Meat</Dropdown.Item>
-          <Dropdown.Item>Animal bybroducts</Dropdown.Item>
+          <Dropdown.Item>Animal byproducts</Dropdown.Item>
           {categories && categories.map(category =>
             <Dropdown.Item key={category.id}>{category.name}</Dropdown.Item>
           )}
