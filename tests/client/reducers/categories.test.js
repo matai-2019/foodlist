d655e65f07b17749513ad7f39dfaf2c07fe4018f
Ashley Foster <ajp.foster@gmail.com>
2019-07-12 14:29:19 +1200

    add getCategoriesSuccess reducer test

---
+++ tests/client/reducers/categories.test.js
@@ -0,0 +1,26 @@
+import {
+  getCategoriesSuccess
+} from '../../../client/actions/categories'
+
+import categoriesReducer from '../../../client/reducers/'
+
+describe('reducer', () => {
+  it('getCatergoriesSuccess', () => {
+    // arrange
+    const testState = [
+      { id: 1, name: 'Fruits' },
+      { id: 2, name: 'Vegetables' },
+      { id: 3, name: 'Grains, beans, and legumes' }
+    ]
+    const currentState = {
+      categories: []
+    }
+    const action = getCategoriesSuccess(testState)
+
+    // act
+    const newState = categoriesReducer(currentState, action)
+
+    // assert
+    expect(newState.categories).toBe(testState)
+  })
+})
