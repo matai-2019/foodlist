83de2faa14bb4903deb3a5f63f7f990ffbbad176
Rahul Lal <Rahul21Lal@gmail.com>
2019-07-12 16:10:27 +1200

    Done the reducer thing, I think

--- client/reducers/foods.js
+++ client/reducers/foods.js
@@ -1,10 +1,19 @@
 import { GET_FOODS_SUCCESS } from './../actions/foods'
+import { GET_CATEGORIES_SUCCESS } from './../actions/categories'
 
 export default function foodReducer (state = [], action) {
   switch (action.type) {
     case GET_FOODS_SUCCESS:
       return action.foods
+    default:
+      return state
+  }
+}
 
+export default function categoryReducer (state = {}, action) {
+  switch (action.type) {
+    case GET_CATEGORIES_SUCCESS:
+      return action.categories
     default:
       return state
   }
--- client/reducers/info.js
+++ client/reducers/info.js
@@ -1,22 +1,31 @@
 import {
   GET_FOODS_PENDING,
   GET_FOODS_SUCCESS,
-  GET_FOODS_ERROR } from './../actions/foods'
+  GET_FOODS_ERROR
+} from './../actions/foods'
 import {
   GET_FOOD_DETAILS_PENDING,
   GET_FOOD_DETAILS_SUCCESS,
-  GET_FOOD_DETAILS_ERROR } from './../actions/foodDetails'
+  GET_FOOD_DETAILS_ERROR
+} from './../actions/foodDetails'
+import {
+  GET_CATEGORIES_PENDING,
+  GET_CATEGORIES_SUCCESS,
+  GET_CATEGORIES_ERROR
+} from './../actions/categories'
 
-export default function infoReducer (state = { pending: true }, action) {
+export default function infoReducer(state = { pending: true }, action) {
   switch (action.type) {
     case GET_FOODS_PENDING:
     case GET_FOOD_DETAILS_PENDING:
+    case GET_CATEGORIES_PENDING:
       return {
         ...state,
         pending: true
       }
     case GET_FOODS_SUCCESS:
     case GET_FOOD_DETAILS_SUCCESS:
+    case GET_CATEGORIES_SUCCESS:
       return {
         ...state,
         pending: false,
@@ -24,6 +33,7 @@ export default function infoReducer (state = { pending: true }, action) {
       }
     case GET_FOODS_ERROR:
     case GET_FOOD_DETAILS_ERROR:
+    case GET_CATEGORIES_ERROR:
       return {
         ...state,
         pending: false,
