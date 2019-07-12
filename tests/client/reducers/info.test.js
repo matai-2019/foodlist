9b3c5d6d9a9f71ad61dbc06c4fe7843534fb0f99
Andre Mayn-Loibl <andremai629@gmail.com>
2019-07-12 15:02:11 +1200

    redux testing

--- tests/client/reducers/info.test.js
+++ tests/client/reducers/info.test.js
@@ -14,7 +14,6 @@ import infoReducer from '../../../client/reducers/'
 
 describe('reducer', () => {
   it('getCatergoriesSuccess', () => {
-    // arrange
     const testState = [
       { id: 1, name: 'Fruits' },
       { id: 2, name: 'Vegetables' },
@@ -25,15 +24,12 @@ describe('reducer', () => {
     }
     const action = getCategoriesSuccess(testState)
 
-    // act
     const newState = infoReducer(currentState, action)
 
-    // assert
     expect(newState.categories).toBe(testState)
   })
 
   it('getCatergoriesPending', () => {
-    // arrange
     const expected = {
       info: {
         pending: true,
@@ -49,15 +45,12 @@ describe('reducer', () => {
     }
     const action = getCategoriesPending()
 
-    // act
     const newState = infoReducer(currentState, action)
 
-    // assert
     expect(newState.info.pending).toBe(expected.info.pending)
     expect(newState.info.error).toBe(expected.info.error)
   })
   it('getCategoriesError', () => {
-    // arrange
     const expected = {
       info: {
         pending: false,
@@ -73,14 +66,11 @@ describe('reducer', () => {
     }
     const action = getCategoriesError('ERR: derp')
 
-    // act
     const newState = infoReducer(currentState, action)
 
-    // assert
     expect(newState.info.error).toBe(expected.info.error)
   })
   it('getFoodsSuccess', () => {
-    // arrange
     const expected = [
       { id: 1, name: 'Lamb', category_id: 5 },
       { id: 2, name: 'Beef', category_id: 5 },
@@ -91,14 +81,11 @@ describe('reducer', () => {
     }
     const action = getFoodsSuccess(expected)
 
-    // act
     const newState = infoReducer(currentState, action)
 
-    // assert
     expect(newState.foods).toBe(expected)
   })
   it('getFoodsPending', () => {
-    // arrange
     const expected = {
       info: {
         pending: true,
@@ -114,11 +101,29 @@ describe('reducer', () => {
     }
     const action = getFoodsPending()
 
-    // act
     const newState = infoReducer(currentState, action)
 
-    // assert
     expect(newState.info.pending).toBe(expected.info.pending)
+    expect(newState.info.error).toBe(expected.info.error)
+  })
+  it('getFoodsError', () => {
+    const expected = {
+      info: {
+        pending: true,
+        error: 'ERR: derp'
+      }
+    }
+
+    const currentState = {
+      info: {
+        pending: false,
+        error: ''
+      }
+    }
+    const action = getFoodsError('ERR: derp')
+
+    const newState = infoReducer(currentState, action)
+
     expect(newState.info.error).toBe(expected.info.error)
   })
 })
