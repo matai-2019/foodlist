65a89e825f3f19a7a4db5c7e948658382e66b2d2
Taine Walford <tainewalford@live.com>
2019-07-12 14:05:36 +1200

    removed duplicate cases in info reducer

--- client/reducers/info.js
+++ client/reducers/info.js
@@ -10,44 +10,25 @@ import {
 export default function infoReducer (state = { pending: true }, action) {
   switch (action.type) {
     case GET_FOODS_PENDING:
-      return {
-        ...state,
-        pending: true
-      }
-
-    case GET_FOODS_SUCCESS:
-      return {
-        ...state,
-        pending: false,
-        error: null
-      }
-
-    case GET_FOODS_ERROR:
-      return {
-        ...state,
-        pending: false,
-        error: action.message
-      }
     case GET_FOOD_DETAILS_PENDING:
       return {
         ...state,
         pending: true
       }
-
+    case GET_FOODS_SUCCESS:
     case GET_FOOD_DETAILS_SUCCESS:
       return {
         ...state,
         pending: false,
         error: null
       }
-
+    case GET_FOODS_ERROR:
     case GET_FOOD_DETAILS_ERROR:
       return {
         ...state,
         pending: false,
         error: action.message
       }
-
     default:
       return state
   }
