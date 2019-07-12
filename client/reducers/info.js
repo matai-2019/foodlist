02786288fa271ea191d7bbe4e795a012eb98cf37
Taine Walford <tainewalford@live.com>
2019-07-12 12:02:06 +1200

    info reducer handles foodDetail actions

--- client/reducers/info.js
+++ client/reducers/info.js
@@ -2,6 +2,10 @@ import {
   GET_FOODS_PENDING,
   GET_FOODS_SUCCESS,
   GET_FOODS_ERROR } from './../actions/foods'
+import {
+  GET_FOOD_DETAILS_PENDING,
+  GET_FOOD_DETAILS_SUCCESS,
+  GET_FOOD_DETAILS_ERROR } from './../actions/foodDetails'
 
 export default function infoReducer (state = { pending: true }, action) {
   switch (action.type) {
@@ -19,6 +23,25 @@ export default function infoReducer (state = { pending: true }, action) {
       }
 
     case GET_FOODS_ERROR:
+      return {
+        ...state,
+        pending: false,
+        error: action.message
+      }
+    case GET_FOOD_DETAILS_PENDING:
+      return {
+        ...state,
+        pending: true
+      }
+
+    case GET_FOOD_DETAILS_SUCCESS:
+      return {
+        ...state,
+        pending: false,
+        error: null
+      }
+
+    case GET_FOOD_DETAILS_ERROR:
       return {
         ...state,
         pending: false,
