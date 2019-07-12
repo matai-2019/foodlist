8d7a0a5143488c95d4d967f4925c81a1cdc69734
Bryce Kehoe <bryce@kehoe.co.nz>
2019-07-12 21:44:57 +1200

    Added infoReducer tests for getCategories actions

--- tests/client/reducers/info.test.js
+++ tests/client/reducers/info.test.js
@@ -2,6 +2,10 @@ import {
   GET_FOOD_DETAILS_PENDING,
   GET_FOOD_DETAILS_SUCCESS,
   GET_FOOD_DETAILS_ERROR } from '../../../client/actions/foodDetails'
+import {
+  GET_CATEGORIES_PENDING,
+  GET_CATEGORIES_SUCCESS,
+  GET_CATEGORIES_ERROR } from '../../../client/actions/categories'
 import infoReducer from '../../../client/reducers/info'
 
 it('returns default state without case matches', () => {
@@ -29,6 +33,29 @@ describe('infoReducer with getFoods actions', () => {
   it('returns error | pending: false, error: message', () => {
     const intialState = { pending: false, error: 'ERROR' }
     const action = { type: GET_FOOD_DETAILS_ERROR, message: 'ERROR' }
+    const actual = infoReducer(intialState, action)
+    expect(actual).toStrictEqual(intialState)
+  })
+})
+
+describe('infoReducer with getCategories actions', () => {
+  it('returns pending | pending: true', () => {
+    const intialState = { pending: true }
+    const action = { type: GET_CATEGORIES_PENDING, action: {} }
+    const actual = infoReducer(intialState, action)
+    expect(actual).toStrictEqual(intialState)
+  })
+
+  it('returns success | pending: false, error: null', () => {
+    const intialState = { pending: false, error: null }
+    const action = { type: GET_CATEGORIES_SUCCESS, action: {} }
+    const actual = infoReducer(intialState, action)
+    expect(actual).toStrictEqual(intialState)
+  })
+
+  it('returns error | pending: false, error: message', () => {
+    const intialState = { pending: false, error: 'ERROR' }
+    const action = { type: GET_CATEGORIES_ERROR, message: 'ERROR' }
     const actual = infoReducer(intialState, action)
     expect(actual).toStrictEqual(intialState)
   })
