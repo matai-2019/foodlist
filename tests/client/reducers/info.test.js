20a40ceccaa445eac80042378a29acd5f75618cf
Bryce Kehoe <bryce@kehoe.co.nz>
2019-07-12 21:39:03 +1200

    Updated name of describe block

--- tests/client/reducers/info.test.js
+++ tests/client/reducers/info.test.js
@@ -4,13 +4,7 @@ import {
   GET_FOOD_DETAILS_ERROR } from '../../../client/actions/foodDetails'
 import infoReducer from '../../../client/reducers/info'
 
-describe('jest', () => {
-  it('jest is working', () => {
-    expect(true).toBe(true)
-  })
-})
-
-describe('infoReducer', () => {
+describe('infoReducer with getFoods actions', () => {
   it('returns default state without case matches', () => {
     const intialState = { pending: true }
     const action = { type: 'GET_NO_CASE_MATCH', action: { name: 'noCaseMatch' } }
