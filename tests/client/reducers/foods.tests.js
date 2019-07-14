b00ad8fa58856f7aeca2963c300c4a6295700715
Rahul Lal <Rahul21Lal@gmail.com>
2019-07-15 10:52:36 +1200

    Removed redundant test

--- tests/client/reducers/foods.tests.js
+++ tests/client/reducers/foods.tests.js
@@ -2,12 +2,6 @@ import { GET_CATEGORY_SUCCESS } from '..//../../client/actions/category'
 import { GET_CATEGORIES_SUCCESS } from '..//../../client/actions/categories'
 import category from '../../../client/reducers/foods'
 
-describe('jest', () => {
-  it('jest is working', () => {
-    expect(true).toBe(true)
-  })
-})
-
 describe('categoryReducer', () => {
   it('returns default state without case matches', () => {
     const action = { type: 'GET_NO_CASE_MATCH', action: { name: 'noCaseMatch'}}
