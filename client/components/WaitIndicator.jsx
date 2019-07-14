5e31f13094794bd49a1f969f9222707a25cffdb9
Jared Tasker <jared@needapart.co.nz>
2019-07-15 11:40:52 +1200

    Refactor WaitIndicator loading message

--- client/components/WaitIndicator.jsx
+++ client/components/WaitIndicator.jsx
@@ -2,17 +2,9 @@ import React from 'react'
 import { Loader } from 'semantic-ui-react';
 
 const WaitIndicator = ({message}) => {
-  const setWaitMessage = () => {
-    if(message){
-      return message
-    }else{
-      return 'Loading'
-    }
-  }
-
   return (
       <Loader size='large' active>
-        {setWaitMessage()}
+        {message || 'Loading'}
       </Loader>
   )
 }
