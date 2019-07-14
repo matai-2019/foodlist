55909d0e2187bdecdd3abb40248d4fcf1c6e01b4
Rahul Lal <Rahul21Lal@gmail.com>
2019-07-15 10:42:47 +1200

    Middle of tests

--- client/reducers/foods.js
+++ client/reducers/foods.js
@@ -1,18 +1,14 @@
 import { GET_FOODS_SUCCESS } from './../actions/foods'
 import { GET_CATEGORIES_SUCCESS } from './../actions/categories'
+import { GET_CATEGORY_SUCCESS } from './../actions/category'
 
 export default function foodReducer (state = [], action) {
   switch (action.type) {
     case GET_FOODS_SUCCESS:
       return action.foods
-    default:
-      return state
-  }
-}
-
-export default function categoryReducer (state = {}, action) {
-  switch (action.type) {
     case GET_CATEGORY_SUCCESS:
+      return action.category
+    case GET_CATEGORIES_SUCCESS:
       return action.category
     default:
       return state
--- tests/client/reducers/foods.tests.js
+++ tests/client/reducers/foods.tests.js
@@ -1,4 +1,5 @@
 import { GET_CATEGORY_SUCCESS } from '..//../../client/actions/category'
+import { GET_CATEGORIES_SUCCESS } from '..//../../client/actions/categories'
 import category from '../../../client/reducers/foods'
 
 describe('jest', () => {
@@ -7,3 +8,14 @@ describe('jest', () => {
   })
 })
 
+describe('categoryReducer', () => {
+  it('returns default state without case matches', () => {
+    const action = { type: 'GET_NO_CASE_MATCH', action: { name: 'noCaseMatch'}}
+    const actual = categoryReducer(undefined, action)
+    expect(actual).toStrictEquals({})
+  })
+
+  it('matches with GET_CATEGORY_SUCCESS', () => {
+    const action = { type: GET_CATEGORY_SUCCESS, categoryDetails: }
+  })
+})
