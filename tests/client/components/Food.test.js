74bf4084d0f984a2a61ea09e6d224a6d9524840c
Jared Tasker <jared@needapart.co.nz>
2019-07-16 10:24:36 +1200

    Test <Food /> passes correct props down to <Label />

--- tests/client/components/Food.test.js
+++ tests/client/components/Food.test.js
@@ -41,3 +41,21 @@ test('<Food /> contains 2 x <Label />', () => {
   const actual = wrapper.find(Label)
   expect(actual).toHaveLength(2)
 })
+
+test('<Food /> passes correct props down to <Label />', () => {
+  const data = {
+    id: 4,
+    name: 'Broccoli',
+    carbonOutput: 5.71,
+    waterUsage: 322
+  }
+  const wrapper = shallow(
+    <Router>
+      <Food food={data}/>
+    </Router>
+  )
+  const carbonOutput = wrapper.children().props().food.carbonOutput
+  const waterUsage = wrapper.children().props().food.waterUsage 
+  expect(carbonOutput).toBe(5.71)
+  expect(waterUsage).toBe(322)
+})
