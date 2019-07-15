dcbff8f5c1f17d33f44ed9cec986c6cdf174f063
Noel Soong <noelsoong@live.com>
2019-07-15 14:49:56 +1200

    Test actions given expected type

--- tests/client/actions/sort.test.js
+++ tests/client/actions/sort.test.js
@@ -1,4 +1,8 @@
-import { SORT_AZ, SORT_CARBON_HIGH, SORT_CARBON_LOW, sortBy } from '../../../client/actions/sort'
+import { SORT_AZ,
+  SORT_CARBON_HIGH,
+  SORT_CARBON_LOW,
+  SORT_WATER_HIGH,
+  sortBy } from '../../../client/actions/sort'
 
 describe('Tests for sort actions', () => {
   it('sortBy(SORT_AZ)  returns a given type SORT_AZ', () => {
@@ -16,6 +20,12 @@ describe('Tests for sort actions', () => {
   it('sortBy(SORT_CARBON_LOW) returns a given type SORT_CARBON_HIGH', () => {
     const action = sortBy(SORT_CARBON_LOW)
     const expected = 'SORT_CARBON_LOW'
+    expect(action.type).toBe(expected)
+  })
+
+  it('sortBy(SORT_WATER_HIGH) returns a given type SORT_CARBON_HIGH', () => {
+    const action = sortBy(SORT_WATER_HIGH)
+    const expected = 'SORT_WATER_HIGH'
     expect(action.type).toBe(expected)
   })
 })
