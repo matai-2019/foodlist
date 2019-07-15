d917be2653018fd8271136094bcfc3d021bb413c
David Brown <david.brown@windowslive.com>
2019-07-15 12:30:20 +1200

    test fixed!

--- tests/client/components/App.test.js
+++ tests/client/components/App.test.js
@@ -1,13 +1,24 @@
 import React from 'react'
-import { shallow } from 'enzyme'
+import { Provider } from 'react-redux'
+import { mount } from 'enzyme'
+import thunkMiddleware from 'redux-thunk'
+import { BrowserRouter } from 'react-router-dom'
+import { applyMiddleware, compose, createStore } from 'redux'
 
 import App from '../../../client/components/App'
+import reducers from '../../../client/reducers'
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(
+  reducers,
+  composeEnhancers(applyMiddleware(thunkMiddleware))
+)
 
 test('<App /> test setup is working correctly', () => {
   expect(true).toBeTruthy()
 })
 
 test('<App /> contains the router', () => {
-  const wrapper = shallow(<App />)
-  expect(wrapper.text()).toBe('<BrowserRouter />')
+  const wrapper = mount(<Provider store={store}><App /></Provider>)
+  expect(wrapper.containsMatchingElement(BrowserRouter)).toBe(true)
 })
