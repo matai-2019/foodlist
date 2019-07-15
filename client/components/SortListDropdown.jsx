2fe4f9e6e8cbf9f11128541f0f7231029aaebb38
Noel Soong <noelsoong@live.com>
2019-07-15 18:32:14 +1200

    Dispatch pulled from development

--- client/components/SortListDropdown.jsx
+++ client/components/SortListDropdown.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { Dropdown, Menu } from 'semantic-ui-react'
-import sortBy from '../actions/sort'
+import { Dropdown } from 'semantic-ui-react'
+import { SORT_AZ,
+  SORT_CARBON_HIGH,
+  SORT_CARBON_LOW, SORT_WATER_HIGH,
+  SORT_WATER_LOW,
+  sortBy } from '../actions/sort'
 
 class SortListDropdown extends React.Component {
   state ={
@@ -9,18 +13,18 @@ class SortListDropdown extends React.Component {
   }
 
   options = [
-    { key: 1, text: 'A-Z', value: 'SORT_AZ' },
-    { key: 2, text: 'Water Usage High-Low', value: 'SORT_WATER_HIGH' },
-    { key: 3, text: 'Water Usage Low-High', value: 'SORT_WATER_LOW' },
-    { key: 4, text: 'Carbon Output High-Low', value: 'CARBON_OUTPUT_HIGH' },
-    { key: 5, text: 'Carbon Output Low-High', value: 'CARBON_OUTPUT_LOW' }
+    { text: 'A-Z', value: SORT_AZ },
+    { text: 'Water Usage High-Low', value: SORT_WATER_HIGH },
+    { text: 'Water Usage Low-High', value: SORT_WATER_LOW },
+    { text: 'Carbon Output High-Low', value: SORT_CARBON_HIGH },
+    { text: 'Carbon Output Low-High', value: SORT_CARBON_LOW }
   ]
 
-  onChangeHandler = (e) => {
+  onChangeHandler = (e, data) => {
     this.setState({
-      text: e.target.value
+      text: e.target.textContent
     })
-    this.props.dispatch(sortBy(e.target.value))
+    this.props.dispatch(sortBy(data.value))
   }
 
   render () {
@@ -32,4 +36,10 @@ class SortListDropdown extends React.Component {
   }
 }
 
-export default connect()(SortListDropdown)
+const mapStateToProps = state => {
+  return {
+    sortType: state.sortType
+  }
+}
+
+export default connect(mapStateToProps)(SortListDropdown)
