c5d4a3a9c7bd97304f314bc52ada706ce51e2726
Jared Tasker <jared@needapart.co.nz>
2019-07-16 10:24:14 +1200

    Test <Food/> contains  2 x <Label/>

--- tests/client/components/Food.test.js
+++ tests/client/components/Food.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
-import { mount } from 'enzyme'
+import { mount, shallow } from 'enzyme'
 
 import Food from '../../../client/components/Food'
 import Label from '../../../client/components/Label'
@@ -9,8 +9,6 @@ import Label from '../../../client/components/Label'
 test('<Food /> test setup is working correctly', () => {
   expect(true).toBeTruthy()
 })
-
-test.skip('<Food /> contains a 2 x <Label />', () => {})
 
 test('<Food/> contains a <Label/>', () => {
   const data = {
@@ -26,4 +24,20 @@ test('<Food/> contains a <Label/>', () => {
   )
   const actual = wrapper.containsMatchingElement(Label)
   expect(actual).toBe(true)
+})
+
+test('<Food /> contains 2 x <Label />', () => {
+  const data = {
+    id: 4,
+    name: 'Broccoli',
+    carbonOutput: 5.71,
+    waterUsage: 322
+  }
+  const wrapper = mount(
+    <Router>
+      <Food food={data}/>
+    </Router>
+  )
+  const actual = wrapper.find(Label)
+  expect(actual).toHaveLength(2)
 })
