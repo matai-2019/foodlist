1e327784d34b572bc21c1a79c68bd83bda99feef
Jared Tasker <jared@needapart.co.nz>
2019-07-15 16:21:01 +1200

    Display Labels next to  foods

--- client/components/Food.jsx
+++ client/components/Food.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import { Table } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
+import Label from './Label';
 
-export default function Food ({ food: { name, id } }) {
+
+export default function Food ({ food: { name, id , carbonOutput, waterUsage} }) {
   return (
     <>
       <Table singleLine>
         <Table.Header>
           <Table.Row >
             <Table.HeaderCell>
-              <Link to={`/details/${id}`}>{name}</Link>
+              <Link to={`/details/${id}`}>
+                {name}
+                <Label value={carbonOutput} type='carbon' />
+                <Label value={waterUsage} type='water' />
+              </Link>
             </Table.HeaderCell>
           </Table.Row>
         </Table.Header>
