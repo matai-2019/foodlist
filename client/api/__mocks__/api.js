db6f4486dd9a9668a704f782aac35d7ccc7e8c6d
Tamari Jacob Gray-Kaiwahia <tamarigray97@gmail.com>
2019-07-15 14:31:36 +1200

    resolved changes

--- client/api/__mocks__/api.js
+++ client/api/__mocks__/api.js
@@ -1,17 +1,7 @@
 export function addFood () {
-  return Promise.resolve({ id: 1 })
+  return Promise.resolve({ body: 'Post ok!' })
 }
 
 export function editFood () {
-  return Promise.resolve({
-    id: 5,
-    name: 'black',
-    category: 'Fish',
-    carbon_output: 5.26,
-    water_usage: null
-  })
-}
-
-export function deleteFood () {
-  return Promise.resolve({ Okay: true })
+  return Promise.resolve({ body: 'Put ok!' })
 }
--- client/api/api.js
+++ client/api/api.js
@@ -1,5 +1,3 @@
-import request from 'superagent'
-
 const foodsUrl = '/api/v1/foods'
 
 export function addFood (food) {
