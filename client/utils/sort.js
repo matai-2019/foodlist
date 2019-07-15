439fe6ecaa15f0f47dd0664f0ac90e408e0591ce
Anisha Kalidas <anishadhana@gmail.com>
2019-07-15 14:19:03 +1200

    PR changes made

---
+++ client/utils/sort.js
@@ -0,0 +1,8 @@
+
+export default function sortAZ(arr) {
+  return arr.sort((a, b) => {
+    if (a.name < b.name) { return -1 }
+    else if (a.name > b.name) { return 1 }
+    else return 0
+  })
+}
