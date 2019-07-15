e8c03a7b127658028750c19de5320e166893f5f4
Taine Walford <tainewalford@live.com>
2019-07-15 12:31:45 +1200

    Made stateful and added onChange and handleSearch

--- client/components/SearchBar.jsx
+++ client/components/SearchBar.jsx
@@ -1,13 +1,28 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import { Input } from 'semantic-ui-react'
+// import { searchFood } from '../actions/searchFood'
 
-export default function SearchBar () {
-  return (
+class SearchBar extends Component {
+  state = {
+    searchedFood: ''
+  }
+  handleChange = evt => {
+    this.setState({ searchedFood: evt.target.value })
+  }
+  handleSearch = () => {
+    // this.props.dispatch(searchFood(this.state.searchedFood))
+  }
+  render = () => {
+    return (
     <>
-      < Input className="ui action input">
-        <input type="text" placeholder="Search..."/>
-        <button className="ui button">Search</button>
+      <Input className="ui action input">
+        <input type="text" placeholder="Search..." onChange={this.handleChange}/>
+        <button className="ui button" onClick={this.handleSearch}>Search</button>
       </Input>
     </>
-  )
+    )
+  }
 }
+
+export default connect()(SearchBar)
