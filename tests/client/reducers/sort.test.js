0f570b7b2206df8e4d10087aaa58694a1ff21a93
Noel Soong <noelsoong@live.com>
2019-07-15 14:50:42 +1200

    Test management of given sortBy(SORT_WATER_HIGH)

--- tests/client/reducers/sort.test.js
+++ tests/client/reducers/sort.test.js
@@ -1,4 +1,7 @@
-import { SORT_AZ, SORT_CARBON_HIGH, SORT_CARBON_LOW } from '../../../client/actions/sort'
+import { SORT_AZ,
+  SORT_CARBON_HIGH,
+  SORT_CARBON_LOW,
+  SORT_WATER_HIGH } from '../../../client/actions/sort'
 import sortReducer from '../../../client/reducers/sort'
 
 describe('Tests given actions into sortReducer', () => {
@@ -41,6 +44,21 @@ describe('Tests given actions into sortReducer', () => {
 
     const action = {
       type: SORT_CARBON_LOW
+    }
+
+    const actual = sortReducer(currentState, action)
+    expect(actual).toBe(expected)
+  })
+
+  it('returns a given sortType SORT_WATER_HIGH', () => {
+    const expected = SORT_WATER_HIGH
+
+    const currentState = {
+      sortType: ''
+    }
+
+    const action = {
+      type: SORT_WATER_HIGH
     }
 
     const actual = sortReducer(currentState, action)
