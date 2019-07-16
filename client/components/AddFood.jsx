5c96cbed34c4debc466fef9fa890920acb61153f
Bryce Kehoe <bryce@kehoe.co.nz>
2019-07-16 16:19:34 +1200

    Re-added redirects that were incorrectly removed

--- client/components/AddFood.jsx
+++ client/components/AddFood.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { Button, Form, Container, Header } from 'semantic-ui-react'
 
@@ -7,6 +8,10 @@ import { addFoodError } from '../actions/updateFoodErrors'
 import { getCategories } from '../actions/categories'
 
 class AddFood extends React.Component {
+  state = {
+    redirect: false
+  }
+
   componentDidMount () {
     this.props.dispatch(getCategories())
   }
@@ -28,48 +33,49 @@ class AddFood extends React.Component {
   }
 
   render () {
-    return (<Container>
-      <div className="ui hidden divider"></div>
-      <Header as="h1">Add a Food</Header>
-      <Form>
-        <Form.Field>
-          <label>Name</label>
-          <input onChange={this.handleInputChange}
-            name="name"
-            placeholder="Enter food name"
-          />
-        </Form.Field>
-        <Form.Field onChange={this.handleInputChange}
-          label="Food group"
-          name="category_id"
-          control="select"
-        >
-          <option value="">Choose a food category</option>
-          {this.props.categories.map(category =>
-            <option key={category.id}
-              value={category.id}>{category.name}</option>
-          )}
-        </Form.Field>
-        <Form.Field>
-          <label>Carbon output</label>
-          <input type="number"
-            onChange={this.handleInputChange}
-            name="carbon_output"
-            placeholder="Enter carbon output"
-          />
-        </Form.Field>
-        <Form.Field>
-          <label>Water usage</label>
-          <input type="number"
-            onChange={this.handleInputChange}
-            name="water_usage"
-            placeholder="Enter water usage"
-          />
-        </Form.Field>
-        <Button onClick={this.handleSubmit} type="submit">Submit</Button>
-      </Form>
-    </Container>
-    )
+    return this.state.redirect
+      ? <Redirect to={{ pathname: '/' }} />
+      : <Container>
+        <div className="ui hidden divider"></div>
+        <Header as="h1">Add a Food</Header>
+        <Form>
+          <Form.Field>
+            <label>Name</label>
+            <input onChange={this.handleInputChange}
+              name="name"
+              placeholder="Enter food name"
+            />
+          </Form.Field>
+          <Form.Field onChange={this.handleInputChange}
+            label="Food group"
+            name="category_id"
+            control="select"
+          >
+            <option value="">Choose a food category</option>
+            {this.props.categories.map(category =>
+              <option key={category.id}
+                value={category.id}>{category.name}</option>
+            )}
+          </Form.Field>
+          <Form.Field>
+            <label>Carbon output</label>
+            <input type="number"
+              onChange={this.handleInputChange}
+              name="carbon_output"
+              placeholder="Enter carbon output"
+            />
+          </Form.Field>
+          <Form.Field>
+            <label>Water usage</label>
+            <input type="number"
+              onChange={this.handleInputChange}
+              name="water_usage"
+              placeholder="Enter water usage"
+            />
+          </Form.Field>
+          <Button onClick={this.handleSubmit} type="submit">Submit</Button>
+        </Form>
+      </Container>
   }
 }
 
