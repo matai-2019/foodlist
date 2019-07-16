ffe89db2eed4ac00a19375b16e695d8173543eb1
Tamari Jacob Gray-Kaiwahia <tamarigray97@gmail.com>
2019-07-16 16:07:43 +1200

    merged with dev

--- client/components/EditFood.jsx
+++ client/components/EditFood.jsx
@@ -15,54 +15,55 @@ class EditFood extends React.Component {
     foodGroup: 'fruits',
     carbonOutput: 101,
     waterUsage: 88,
-    redirect: false
+    redirect: false,
+    foodDetails: this.props.foodDetails
   }
 
   componentDidMount() {
-    const { foodDetails, dispatch, match: { params: { foodId } } } = this.props
+    const { dispatch, match: { params: { foodId } } } = this.props
     Number(foodId)
     dispatch(getFood(foodId))
-    this.setState({ foodDetails })
   }
 
-    handleChange = (e) => {
-      this.setState({
-        [e.target.name]: e.target.value
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  handleSubmit = () => {
+    return editFood(this.state)
+      .then(() => this.setState({
+        redirect: true
+      }))
+      .catch(err => {
+        this.props.dispatch(editFoodError(err.message))
       })
-    }
-
-    handleSubmit = () => {
-      return editFood(this.state)
-        .then(() => this.setState({
-          redirect: true
-        }))
-        .catch(err => {
-          this.props.dispatch(editFoodError(err.message))
-        })
-    }
-
-    render() {
-      return this.state.redirect ? (<Redirect to={{ pathname: '/' }} />) : (
-        <>
-          <Header as="h1">Edit Food</Header>
-          <Form>
-            <Form.Group widths='equal'>
-              <Form.Field type="text" name="name" control={Input} label='Name :' value={this.state.name} />
-              <Form.Field type="text" name="foodGroup" control={Select} label='Food Group:' />
-            </Form.Group>
-            <Form.Group widths='equal'>
-              <Form.Field type="number" name="carbonOutput" label='Carbon Output: ' control={Input} value={this.state.carbonOutput} onChange={this.handleChange} />
-              <Form.Field type="number" name="waterUsage" label='Water Usage: ' control={Input} value={this.state.waterUsage} onChange={this.handleChange} />
-            </Form.Group>
-
-            <Button type='submit'>Submit</Button>
-          </Form>
-        </>
-      )
-    }
-  }
-  const mapStateToProps = (state) => {
-    return state
   }
 
-  export default connect(mapStateToProps)(EditFood)
+  render() {
+    const { name, carbonOutput, waterUsage } = this.state.foodDetails
+    return this.state.redirect ? (<Redirect to={{ pathname: '/' }} />) : (
+      <>
+        <Header as="h1">Edit Food</Header>
+        <Form>
+          <Form.Group widths='equal'>
+            <Form.Field type="text" name="name" control={Input} label='Name :' value={name} />
+            <Form.Field type="text" name="foodGroup" control={Select} label='Food Group:' />
+          </Form.Group>
+          <Form.Group widths='equal'>
+            <Form.Field type="number" name="carbonOutput" label='Carbon Output: ' control={Input} value={carbonOutput} onChange={this.handleChange} />
+            <Form.Field type="number" name="waterUsage" label='Water Usage: ' control={Input} value={waterUsage} onChange={this.handleChange} />
+          </Form.Group>
+
+          <Button type='submit'>Submit</Button>
+        </Form>
+      </>
+    )
+  }
+}
+const mapStateToProps = (state) => {
+  return state
+}
+
+export default connect(mapStateToProps)(EditFood)
