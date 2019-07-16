7ce314e035c685e1224f6fa97993a3fbc0212edf
Anisha Dhana <anishadhana@gmail.com>
2019-07-16 16:01:59 +1200

    replaced Link with Redirect

--- client/components/Food.jsx
+++ client/components/Food.jsx
@@ -1,24 +1,49 @@
 import React from 'react'
 import { Table } from 'semantic-ui-react'
-import { Link } from 'react-router-dom'
+import { Redirect } from 'react-router-dom'
 import Label from './Label';
 
-export default function Food ({ food: { name, id , carbonOutput, waterUsage} }) {
-  return (
-    <>
-      <Table singleLine>
-        <Table.Header>
-          <Table.Row >
-            <Table.HeaderCell>
-              <Link to={`/details/${id}`}>
+class Food extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      redirect: null
+    }
+  }
+
+  handleClick = id => {
+    return () => {
+      this.setState({ redirect: Number(id) })
+    }
+  }
+
+  renderRedirect = () => {
+    if (this.state.redirect) {
+      return <Redirect push to={`/details/${this.state.redirect}`} />
+    }
+  }
+
+  render() {
+    const { id, name, carbonOutput, waterUsage } = this.props.food
+    return (
+      <>
+        {this.renderRedirect()}
+        < Table singleLine onClick={this.handleClick(id)} >
+          <Table.Header>
+            <Table.Row >
+              <Table.HeaderCell>
                 {name}
                 <Label value={carbonOutput} type="carbon" />
                 <Label value={waterUsage} type="water" />
-              </Link>
-            </Table.HeaderCell>
-          </Table.Row>
-        </Table.Header>
-      </Table>
-    </>
-  )
+              </Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+        </Table >
+      </>
+    )
+  }
+
 }
+
+export default Food
