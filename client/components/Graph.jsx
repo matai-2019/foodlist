6915bd125966444bceb385b214ec40bb249e7caa
Ashley Foster <ajp.foster@gmail.com>
2019-07-16 16:09:46 +1200

    add graph component

--- client/components/Graph.jsx
+++ client/components/Graph.jsx
@@ -1,18 +1,41 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import {
-  BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
-} from 'recharts';
+import { HorizontalBar } from 'react-chartjs-2'
 
 class Graph extends React.Component {
-
-  data = this.props.foods.map(food => {
-    { name: food.name, }
-  })
+  data = {
+    labels: this.props.foods.map(food => {
+      return food.name
+    }),
+    datasets: [
+      {
+        label: 'Carbon Output (Kilogram / 10000 Calories)',
+        backgroundColor: 'red',
+        hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+        hoverBorderColor: 'rgba(255,99,132,1)',
+        data: this.props.foods.map(food => {
+          return food.carbonOutput
+        })
+      },
+      {
+        label: 'Water Usage (Litres / Kilogram)',
+        backgroundColor: 'green',
+        hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+        hoverBorderColor: 'rgba(255,99,132,1)',
+        data: this.props.foods.map(food => {
+          return food.waterUsage
+        })
+      }
+    ]
+  }
 
   render () {
     return (
-      name
+      <>
+      <div className="chart-container">
+        <HorizontalBar data={ this.data }/>
+      </div>
+      </>
     )
   }
 }
