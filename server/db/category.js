65badd2e9843adf44299992248930ce184a29585
Bryce Kehoe <bryce@kehoe.co.nz>
2019-07-26 18:52:21 +1200

    Add joins to getFoodsByCategory has all the food details

--- server/db/category.js
+++ server/db/category.js
@@ -7,10 +7,14 @@ function getCategories (db = connection) {
 function getFoodsByCategory (categoryName, db = connection) {
   return db('foods')
     .join('categories', 'foods.category_id', 'categories.id')
+    .join('carbon_outputs', 'foods.id', 'carbon_outputs.food_id')
+    .join('water_usages', 'foods.id', 'water_usages.food_id')
     .where('categories.name', categoryName)
     .select(
       'foods.id',
-      'foods.name'
+      'foods.name',
+      'carbon_outputs.value as carbonOutput',
+      'water_usages.value as waterUsage'
     )
 }
 
